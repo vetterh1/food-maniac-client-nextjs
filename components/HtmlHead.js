0440b7b7d503d36073b2ee08cba29f7f618a38fe
Laurent VETTERHOEFFER <vetterh1@yahoo.fr>
2018-12-14 11:24:37 +0100

    minor changes

---
+++ components/HtmlHead.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+// <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossOrigin="anonymous" />
+// <link rel="stylesheet" href="https://unpkg.com/papercss/dist/paper.min.css" />
+// <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+
+const HtmlHead = () => (
+  <Head>
+    <title>Food Maniac (next.js version) 2</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>                
+    <meta name="description" content="Know the best pizza in town? Share it!"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1"/>
+  </Head>
+)
+
+export default HtmlHead
--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,6 +1,6 @@
 import  "../components/themes/bootswatch_sketchy.min.css"
 import ThemeProvider, { ThemeConsumer, themes } from '../components/themes/ThemeProvider.js'
-import Head from 'next/head'
+import HtmlHead from './HtmlHead'
 import Header from './Header'
 import Footer from './Footer';
 import Info from './Info';
@@ -25,18 +25,10 @@ class Layout extends React.Component {
     const containerClasses = "layout container";
     return(
       <ThemeProvider>
-          <Head>
-            <title>Food Maniac (next.js version)</title>
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <HtmlHead />
 
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>                
-            <meta name="description" content="Know the best pizza in town? Share it!"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, maximum-scale=1, minimum-scale=1"/>
-          </Head>
           <Header />
-
+          
           <div className={containerClasses}>
             <Info />
             <Message />
@@ -54,7 +46,10 @@ class Layout extends React.Component {
             }
 
             .header {
+              margin-top: 8px;
               margin-bottom: 30px;
+              margin-left: 8px;
+              margin-right: 8px;
             }
 
             .paper {
